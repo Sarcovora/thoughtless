88f1dd7777ffd303d586fb28d0a61ad130e345df
Evan Kuo <evan.kuo.edu@gmail.com>
2024-02-07 20:10:56 -0600

    dummy navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+
+import { NextUIProvider } from "@nextui-org/react";
+
 function App() {
   return (
-    <div class="flex justify-center items-center h-screen">
-      <h1 class="text-2xl font-bold">thoughtless.</h1>
-    </div>
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">thoughtless.</p>
+        </NavbarBrand>
+        <NavbarContent
+          className="hidden sm:flex gap-4"
+          justify="center"
+        ></NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem isActive className="pr-4">
+            <Link color="foreground" href="#">
+              Dashboard
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+      <div class="flex justify-center items-center h-screen">
+        <h1 class="text-2xl font-bold">thoughtless.</h1>
+      </div>
+    </>
   );
 }
 

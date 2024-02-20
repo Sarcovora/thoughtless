8d8185e4f73db4ca40c65dc714d3addbe26e313f
Andy Friedrich <andyfriedrich210@gmail.com>
2024-02-19 21:12:18 -0600

    bruh

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -1,33 +1,82 @@
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue} from "@nextui-org/react";
+import {EditIcon} from "./EditIcon";
+import {DeleteIcon} from "./DeleteIcon";
+import {EyeIcon} from "./EyeIcon";
+import {columns, users} from "./data";
 
-const columns = [
-    { key: "name", label: "Name" },
-    { key: "phone", label: "Phone Number" },
-    { key: "favorite_color", label: "Favorite Color" },
-  ];
-  
-  const rows = [
-    { id: 1, name: "Bob", phone: "123-456-7890", favorite_color: "Red" },
-    { id: 2, name: "Joe", phone: "987-654-3210", favorite_color: "Orange" },
-    { id: 3, name: "Sue", phone: "555-123-4567", favorite_color: "Blue" },
-    // Add more rows as needed
-  ];
-  
+const statusColorMap = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
 
 export default function TableView() {
+  const renderCell = React.useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{radius: "lg", src: user.avatar}}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
   return (
-    <Table aria-label="Example table with dynamic content">
-      <TableHeader>
-        {columns.map((column) =>
-          <TableColumn key={column.key}>{column.label}</TableColumn>
+  <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
         )}
       </TableHeader>
-      <TableBody>
-        {rows.map((row) =>
-          <TableRow key={row.id}>
-            {columns.map((column) =>
-              <TableCell key={`${row.id}-${column.key}`}>{row[column.key]}</TableCell>
-            )}
+      <TableBody items={users}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
           </TableRow>
         )}
       </TableBody>

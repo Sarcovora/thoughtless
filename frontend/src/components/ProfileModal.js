3a3ba844b4b3d202c7f48e7563b3b13ba8547e2f
Evan Kuo <evan.kuo.edu@gmail.com>
2024-03-01 13:40:30 -0600

    profile view popup

---
+++ frontend/src/components/ProfileModal.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Divider,
+    Link,
+    Image,
+    Chip,
+    Select,
+    User,
+    Avatar,
+    SelectItem,
+    Accordion,
+    AccordionItem,
+    Slider,
+	Textarea,
+} from '@nextui-org/react';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import NavigationBar from './Navigation';
+
+import { columns, users } from './data'; // Import user data
+import RubricRatings from './RubricRatings';
+
+const statusColorMap = {
+    active: 'success',
+    paused: 'danger',
+    vacation: 'warning',
+};
+
+const ProfileModal = (selectedUser) => {
+    selectedUser = selectedUser.selectedUser;
+
+    return (
+        <>
+            {/* <MaxWidthWrapper> */}
+            {selectedUser ? (
+                <>
+                    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+                        <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+                            {/* left side */}
+                            {/* <div className="flex-1 xl:flex"> */}
+                            <div className="flex-1 xl:flex">
+                                <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+                                    <Card>
+                                        <CardHeader className="flex gap-3 justify-center flex-col">
+                                            <Image
+                                                alt="User avatar"
+                                                height={60}
+                                                radius="md"
+                                                src={selectedUser.avatar}
+                                                width={60}
+                                            />
+
+                                            <h1 className="text-2xl font-bold">
+                                                {selectedUser.name}
+                                            </h1>
+
+                                            <div className="flex flex-col">
+                                                <p className="text-small text-default-500">
+                                                    {selectedUser.email}
+                                                </p>
+                                            </div>
+                                        </CardHeader>
+                                        <Divider />
+                                        <CardBody>
+                                            <p className="mb-1 font-semibold">
+                                                Role:
+                                            </p>
+                                            <p className="mb-3">
+                                                {selectedUser.role}
+                                            </p>
+
+                                            <Divider className="mb-3" />
+
+                                            <p className="mb-1 font-semibold">
+                                                Team:
+                                            </p>
+                                            <p className="mb-3">
+                                                {selectedUser.team}
+                                            </p>
+
+                                            <Divider className="mb-3" />
+
+                                            <p className="mb-1 font-semibold">
+                                                Status:
+                                            </p>
+
+                                            <Chip
+                                                className="capitalize"
+                                                color={
+                                                    statusColorMap[
+                                                        selectedUser.status
+                                                    ]
+                                                }
+                                                size="md"
+                                                variant="flat"
+                                            >
+                                                {selectedUser.status}
+                                            </Chip>
+                                        </CardBody>
+                                        <Divider />
+                                        <CardFooter>
+                                            <Link
+                                                isExternal
+                                                showAnchorIcon
+                                                href={
+                                                    selectedUser.socials
+                                                        .linkedin
+                                                }
+                                            >
+                                                LinkedIn
+                                            </Link>
+                                            <Link
+                                                isExternal
+                                                showAnchorIcon
+                                                href={
+                                                    selectedUser.socials.github
+                                                }
+                                            >
+                                                Github
+                                            </Link>
+                                        </CardFooter>
+                                    </Card>
+                                </div>
+                            </div>
+
+                            {/* right side */}
+                            {/* <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t0"> */}
+                            <div className="shrink-0 flex-[0.4] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t0">
+                                <Accordion selectionMode="multiple">
+                                    <AccordionItem
+                                        key="1"
+                                        aria-label="Question 1"
+                                        title="Question 1"
+                                    >
+                                        <RubricRatings />
+                                    </AccordionItem>
+                                    <AccordionItem
+                                        key="2"
+                                        aria-label="Question 2"
+                                        title="Question 2"
+                                    >
+                                        <RubricRatings />
+                                    </AccordionItem>
+                                    <AccordionItem
+                                        key="3"
+                                        aria-label="Question 3"
+                                        title="Question 3"
+                                    >
+                                        <RubricRatings />
+                                    </AccordionItem>
+                                </Accordion>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h1 className="text-l font-extralight">
+                        Please select a user
+                    </h1>
+                </>
+            )}
+            {/* </MaxWidthWrapper> */}
+        </>
+    );
+};
+
+export default ProfileModal;
--- frontend/src/components/TableView.js
+++ frontend/src/components/TableView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Table,
     TableHeader,
@@ -10,6 +10,15 @@ import {
     Chip,
     Tooltip,
     getKeyValue,
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    useDisclosure,
+    RadioGroup,
+    Radio,
 } from '@nextui-org/react';
 import { EditIcon } from './EditIcon';
 import { DeleteIcon } from './DeleteIcon';
@@ -18,6 +27,7 @@ import { columns, users } from './data';
 
 import NavigationBar from './Navigation';
 import MaxWidthWrapper from './MaxWidthWrapper';
+import ProfileModal from './ProfileModal';
 
 const statusColorMap = {
     active: 'success',
@@ -26,23 +36,31 @@ const statusColorMap = {
 };
 
 export default function TableView() {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleUserClick = (user) => {
+        onOpen();
+        setSelectedUser(user);
+    };
+
     const renderCell = React.useCallback((user, columnKey) => {
         const cellValue = user[columnKey];
 
         switch (columnKey) {
             case 'name':
                 return (
-                    <User
-                        avatarProps={{ radius: 'lg', src: user.avatar }}
-                        description={user.email}
-                        name={cellValue}
-                    >
-                        {user.email}
-                    </User>
+                    <div onClick={() => handleUserClick(user)} className="flex flex-col items-start cursor-pointer hover:bg-gray-200 p-2 rounded-xl">
+                        <User
+                            avatarProps={{ radius: 'lg', src: user.avatar }}
+                            description={user.email}
+                            name={cellValue}
+                        ></User>
+                    </div>
                 );
             case 'role':
                 return (
-                    <div className="flex flex-col">
+                    <div className="flex flex-col items-start">
                         <p className="text-bold text-sm capitalize">
                             {cellValue}
                         </p>
@@ -53,35 +71,37 @@ export default function TableView() {
                 );
             case 'status':
                 return (
-                    <Chip
-                        className="capitalize"
-                        color={statusColorMap[user.status]}
-                        size="sm"
-                        variant="flat"
-                    >
-                        {cellValue}
-                    </Chip>
-                );
-            case 'actions':
-                return (
-                    <div className="relative flex items-center gap-2">
-                        <Tooltip content="Details">
-                            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                <EyeIcon />
-                            </span>
-                        </Tooltip>
-                        <Tooltip content="Edit user">
-                            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                <EditIcon />
-                            </span>
-                        </Tooltip>
-                        <Tooltip color="danger" content="Delete user">
-                            <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                                <DeleteIcon />
-                            </span>
-                        </Tooltip>
+                    <div className="flex flex-col items-start">
+                        <Chip
+                            className="capitalize"
+                            color={statusColorMap[user.status]}
+                            size="sm"
+                            variant="flat"
+                        >
+                            {cellValue}
+                        </Chip>
                     </div>
                 );
+            // case 'actions':
+            //     return (
+            //         <div className="relative flex items-center gap-2">
+            //             <Tooltip content="Details">
+            //                 <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+            //                     <EyeIcon />
+            //                 </span>
+            //             </Tooltip>
+            //             <Tooltip content="Edit user">
+            //                 <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+            //                     <EditIcon />
+            //                 </span>
+            //             </Tooltip>
+            //             <Tooltip color="danger" content="Delete user">
+            //                 <span className="text-lg text-danger cursor-pointer active:opacity-50">
+            //                     <DeleteIcon />
+            //                 </span>
+            //             </Tooltip>
+            //         </div>
+            //     );
             default:
                 return cellValue;
         }
@@ -90,32 +110,72 @@ export default function TableView() {
     return (
         <>
             <NavigationBar />
-            <MaxWidthWrapper >
-            <Table aria-label="Example table with custom cells">
-                <TableHeader columns={columns}>
-                    {(column) => (
-                        <TableColumn
-                            key={column.uid}
-                            align={
-                                column.uid === 'actions' ? 'center' : 'start'
-                            }
-                        >
-                            {column.name}
-                        </TableColumn>
-                    )}
-                </TableHeader>
-                <TableBody items={users}>
-                    {(item) => (
-                        <TableRow key={item.id}>
-                            {(columnKey) => (
-                                <TableCell>
-                                    {renderCell(item, columnKey)}
-                                </TableCell>
+            <MaxWidthWrapper>
+                <Table aria-label="Table of all applicant information">
+                    <TableHeader columns={columns}>
+                        {(column) => (
+                            <TableColumn
+                                key={column.uid}
+                                // align={
+                                //     column.uid === 'actions'
+                                //         ? 'center'
+                                //         : 'start'
+                                // }
+                            >
+                                {column.name}
+                            </TableColumn>
+                        )}
+                    </TableHeader>
+                    <TableBody items={users}>
+                        {(item) => (
+                            <TableRow key={item.id}>
+                                {(columnKey) => (
+                                    <TableCell>
+                                        {renderCell(item, columnKey)}
+                                    </TableCell>
+                                )}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+                <div className="flex flex-col gap-2">
+                    {/* <Button onPress={onOpen}>Open Modal</Button> */}
+                    <Modal
+                        isOpen={isOpen}
+                        onOpenChange={onOpenChange}
+                        scrollBehavior={'inside'}
+                        size="full"
+                    >
+                        <ModalContent>
+                            {(onClose) => (
+                                <>
+                                    <ModalHeader className="flex flex-col gap-1">
+                                        {selectedUser ? (selectedUser.name) : 'No user selected'}
+                                    </ModalHeader>
+                                    <ModalBody>
+                                        {selectedUser ? <ProfileModal selectedUser={selectedUser} /> : "No user selected"}
+                                    </ModalBody>
+                                    <ModalFooter>
+                                        {/* <Button
+                                            color="danger"
+                                            variant="light"
+                                            onPress={onClose}
+                                        >
+                                            Close
+                                        </Button> */}
+                                        <Button
+                                            color="primary"
+                                            size='md'
+                                            onPress={onClose}
+                                        >
+                                            Done
+                                        </Button>
+                                    </ModalFooter>
+                                </>
                             )}
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        </ModalContent>
+                    </Modal>
+                </div>
             </MaxWidthWrapper>
         </>
     );

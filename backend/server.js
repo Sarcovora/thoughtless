8fbd2798683e77fc496a3e9bc2945476c8cb05b5
Andy Friedrich <andyfriedrich210@gmail.com>
2024-03-02 13:59:41 -0600

    questions endpoint

--- backend/server.js
+++ backend/server.js
@@ -75,6 +75,28 @@ app.get("/orgs", async (req, res) => {
     }
 });
 
+// GET: List of questions for an org
+app.get("/questions", async(req, res) => {
+    try {
+        const {org} = req.body;
+
+        const orgSnapshot = await db.collection(ORG_COLLECTION).where("name", "==", org).get(); // FIXME should change this to be ID 
+
+        if (orgSnapshot.empty) {
+            console.log('No matching documents.');
+            res.status(404).send('No matching documents.');
+            return;
+        }  
+
+        // Access the qeustions list within the org document
+        const questions = orgSnapshot.docs[0].data().questions; // Assuming there's only one org with this name
+    
+        res.status(200).send(questions);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
 // POST: Endpoint to make a new reviewer and assign them to their org ... 
 app.post("/reviewer", async (req, res) => {
     try {
@@ -246,136 +268,6 @@ app.get("/feedback", async (req, res) => {
         res.status(500).send(error.message);
     }
 });
-
-
-// const MAIN_COLLECTION_NAME = 'orgs'
-
-// // Define your API function to add org
-// async function addOrg(orgName) {
-//     try {
-//       // Add main collection
-//       await firestore.collection(MAIN_COLLECTION_NAME).doc(orgName).set({
-//         // Add fields or data for main collection
-//         name: orgName
-//       });
-  
-//       // Add subcollections
-//       await firestore.collection(MAIN_COLLECTION_NAME).doc(orgName).collection('reviewers').doc('subDoc1').set({
-//         // Add fields or data for subcollection 1
-//         subField1: 'subValue1',
-//         subField2: 'subValue2'
-//       });
-  
-//       await firestore.collection('mainCollection').doc('mainDoc').collection('subCollection2').doc('subDoc2').set({
-//         // Add fields or data for subcollection 2
-//         subField3: 'subValue3',
-//         subField4: 'subValue4'
-//       });
-  
-//       // Add more subcollections as needed
-  
-//       console.log("Collections added successfully!");
-//     } catch (error) {
-//       console.error("Error adding collections: ", error);
-//     }
-//   }
-
-// // POST: Endpoint to add a new org
-// app.post("/org", async (req, res) => {
-//     try {
-//       const {name, questions} = req.body; 
-//       const data = {
-//         name, 
-//         questions
-//       }
-//       const addedTask = await db.collection("orgs").add(data);    // You can also add validation for the task data here
-  
-//       // Sending a successful response with the ID of the newly created org
-//       res.status(201).send({ id: addedTask.id, ...data });
-//     } catch (error) {
-//       // Sending an error response in case of an exception
-//       res.status(500).send(error.message);
-//     }
-// });
-
-// // GET: Endpoint to retrieve all orgs
-// app.get("/orgs", async (req, res) => {
-//     try {
-//         const snapshot = await db.collection("orgs").get();
-
-//         let orgs = [];
-//         if (snapshot.empty) {
-//             console.log('No matching documents.');
-//             res.status(404).send('No matching documents.');
-//             return;
-//         }  
-
-//         snapshot.forEach((doc) => {
-//             orgs.push({
-//                 id: doc.id,
-//                 ...doc.data()
-//             });
-//         });
-
-//         res.status(200).send(orgs);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
-
-// // POST: Endpoint to add a new applicant
-// app.post("/application", async (req, res) => {
-//     try {
-        
-//         const {name, org, ...responses } = req.body;
-        
-//         const newData = {
-//             name: name,
-//             responses: Object.entries(responses).map(([question, answer]) => ({ question, answer })), 
-//             org: org
-//         };
-        
-//         console.log(newData);
-        
-//         const data = {
-//             name, 
-//             responses, 
-//             org
-//         };
-//       const addedTask = await db.collection("applications").add(data); 
-  
-//       // Sending a successful response with the ID of the newly created applicant
-//       res.status(201).send({ id: addedTask.id, ...data });
-//     } catch (error) {
-//       // Sending an error response in case of an exception
-//       res.status(500).send(error.message);
-//     }
-// });
-
-// // GET: Endpoint to retrieve all applications
-// app.get("/applications", async (req, res) => {
-//     try {
-//         const snapshot = await db.collection("applications").get();
-
-//         let apps = [];
-//         if (snapshot.empty) {
-//             console.log('No matching documents.');
-//             res.status(404).send('No matching documents.');
-//             return;
-//         }  
-
-//         snapshot.forEach((doc) => {
-//             apps.push({
-//                 id: doc.id,
-//                 ...doc.data()
-//             });
-//         });
-
-//         res.status(200).send(apps);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
 
 // Setting the port for the server to listen on
 const PORT = process.env.PORT || 4001;

9e5577a53ffcc13393bec57bb13ef5f1f718352d
Evan Kuo <evan.kuo.edu@gmail.com>
2024-03-17 14:42:51 -0500

    card for signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
-import { Card, Spacer, Button, Input, Row, Checkbox } from '@nextui-org/react';
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Divider,
+    Spacer,
+    Button,
+    Input,
+    Row,
+    Checkbox,
+} from '@nextui-org/react';
 import { EyeFilledIcon } from './EyeFilledIcon';
 import { EyeSlashFilledIcon } from './EyeSlashFilledIcon';
 import TwoThirdsWidthWrapper from './twoThirdsWidthWrapper';
 import NavigationBar from './Navigation';
+
+// import backgroundSvg from '../assets/topographical.svg';
 
 export default function Signup() {
     const [firstName, setFirstName] = useState('');
@@ -26,52 +38,82 @@ export default function Signup() {
     };
     return (
         <div>
+            {/* <style>{` */}
+            {/*     body { */}
+            {/*         background-image: url(${backgroundSvg}); */}
+            {/*         background-repeat: no-repeat; */}
+            {/*         background-size: cover; */}
+            {/*         background-position: center; */}
+            {/*     } */}
+            {/**/}
+            {/*     .card-container { */}
+            {/*         background-color: transparent; */}
+            {/*     } */}
+            {/* `}</style> */}
             <NavigationBar />
             <TwoThirdsWidthWrapper>
-                <h1 className="text-4xl font-bold text-center mt-10">
-                    Sign Up
-                </h1>
-                <p className="py-2 font-light">Welcome to the family!</p>
-                <Spacer y={7} />
-                <div className="flex">
-                    <Input
-                        placeholder="Your first name"
-                        label="First Name"
-                        variant="bordered"
-                    />
-                    <Spacer x={5} />
-                    <Input
-                        placeholder="Your last name"
-                        label="Last Name"
-                        variant="bordered"
-                    />
-                </div>
-                <Spacer y={5} />
-                <Input
-                    placeholder="Your email"
-                    label="Email"
-                    variant="bordered"
-                />
-                <Spacer y={5} />
-                <Input
-                    label="Password"
-                    variant="bordered"
-                    placeholder="Enter your password"
-                    endContent={
-                        <button
-                            className="focus:outline-none"
-                            type="button"
-                            onClick={toggleVisibility}
-                        >
-                            {isVisible ? (
-                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                            ) : (
-                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                            )}
-                        </button>
-                    }
-                    type={isVisible ? 'text' : 'password'}
-                />
+                <Card
+                // isBlurred
+                // className="border-none bg-background/60 dark:bg-default-100/50"
+                // shadow="sm"
+                >
+                    <h1 className="text-4xl font-bold text-center mt-10">
+                        Sign Up
+                    </h1>
+                    <p className="py-2 font-light">Welcome to the family!</p>
+                    <CardBody>
+                        <div className="flex">
+                            <Input
+                                placeholder="Your first name"
+                                label="First Name"
+                                variant="bordered"
+                            />
+                            <Spacer x={5} />
+                            <Input
+                                placeholder="Your last name"
+                                label="Last Name"
+                                variant="bordered"
+                            />
+                        </div>
+                        <Spacer y={5} />
+                        <Input
+                            placeholder="Your email"
+                            label="Email"
+                            variant="bordered"
+                        />
+                        <Spacer y={5} />
+                        <Input
+                            label="Password"
+                            variant="bordered"
+                            placeholder="Enter your password"
+                            endContent={
+                                <button
+                                    className="focus:outline-none"
+                                    type="button"
+                                    onClick={toggleVisibility}
+                                >
+                                    {isVisible ? (
+                                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    ) : (
+                                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    )}
+                                </button>
+                            }
+                            type={isVisible ? 'text' : 'password'}
+                        />
+                        <Spacer y={5} />
+                        <div className="ml-auto">
+                            <Button
+                                className="mx-auto max-w-[66.666%] bg-black text-white"
+                                variant="flat"
+                                size="md"
+                                color="primary"
+                            >
+                                Sign up
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
             </TwoThirdsWidthWrapper>
         </div>
     );

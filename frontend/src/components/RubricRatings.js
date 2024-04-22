3b942dee0cf4e340e7cdd910672a4c6d31639017
Evan Kuo <evan.kuo.edu@gmail.com>
2024-04-22 17:48:50 -0500

    FUCKALL

--- frontend/src/components/RubricRatings.js
+++ frontend/src/components/RubricRatings.js
@@ -6,16 +6,22 @@ import {
     Select,
     SelectItem,
 } from '@nextui-org/react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {} from '@nextui-org/react';
 import { ListboxWrapper } from './ListboxWrapper';
 
-const RubricRatings = ({ feedback, questionId, applicant }) => {
+const RubricRatings = ({ feedback, questionId, applicant, ratings, setRatings, comments, setComments }) => {
     // const ratings = feedback.feedbackArray;
     // const comments = feedback.commentsArray;
 
-    const [ratings, setRatings] = useState(feedback.feedbackArray);
-    const [comments, setComments] = useState(feedback.commentsArray);
+    // const [ratings, setRatings] = useState(feedback.feedbackArray);
+    // const [comments, setComments] = useState(feedback.commentsArray);
+    let globalComments = comments;
+
+    // let ratings = feedback.feedback_array;
+    // let comments = feedback.comments_array;
+    // console.log("RATINGS", ratings)
+    // console.log("COMMENTS", comments)
 
     // console.log(ratings);
     // console.log(comments)
@@ -31,33 +37,34 @@ const RubricRatings = ({ feedback, questionId, applicant }) => {
 
     const backendURL = 'https://thoughtless-backend.vercel.app';
 
-    function updateFeedback() {
-        // console.log(reviewer);
-        // console.log(org);
-        // console.log(applicant);
-        // console.log(ratings);
-        // console.log(comments);
-        const fetchURL = `${backendURL}/feedback`;
-        console.log(fetchURL)
-        // ratings is a 2d array. Unpack that into a 1d array
-        const flatRatings = ratings.flat();
-        // console.log(fetchURL)
-        const body = JSON.stringify({
-            reviewer: reviewer,
-            org: org,
-            app: applicant,
-            feedback_array: flatRatings,
-            comments_array: comments,
-        });
-        console.log(body);
-        fetch(fetchURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json', // Set the headers to inform the server about the type of the content
-            },
-            body: body,
-        });
-    }
+    // function updateFeedback() {
+    //     // console.log(reviewer);
+    //     // console.log(org);
+    //     // console.log(applicant);
+    //     // console.log(ratings);
+    //     // console.log(comments);
+    //     const fetchURL = `${backendURL}/feedback`;
+    //     console.log(fetchURL)
+    //     // ratings is a 2d array. Unpack that into a 1d array
+    //     const flatRatings = ratings.flat();
+    //     // console.log(fetchURL)
+    //     console.log ("POSTING COMMENTS", comments)
+    //     const body = JSON.stringify({
+    //         reviewer: reviewer,
+    //         org: org,
+    //         app: applicant,
+    //         feedback_array: flatRatings,
+    //         comments_array: comments,
+    //     });
+    //     // console.log(body);
+    //     fetch(fetchURL, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json', // Set the headers to inform the server about the type of the content
+    //         },
+    //         body: body,
+    //     });
+    // }
     // const [selectedKeys, setSelectedKeys] = React.useState(new Set(['text']));
 
     // const selectedValue = React.useMemo(
@@ -67,10 +74,19 @@ const RubricRatings = ({ feedback, questionId, applicant }) => {
 
     const handleCommentsChange = (e) => {
         const newComments = [...comments]; // Create a copy of the comments array
+        // const newComments = [...globalComments]; // Create a copy of the comments array
+        // console.log(newComments)
         newComments[questionId] = e.target.value; // Update the value for the specific questionId
+        // globalComments = newComments
         setComments(newComments); // Update the state
-        updateFeedback();
+        comments[questionId] = e.target.value;
+        // console.log("COMMMNETS", comments)
+        // updateFeedback();
     };
+
+    // useEffect(() => {
+    //     updateFeedback();
+    // }, [ratings, comments]);
 
     const rubric = [
         {
@@ -118,12 +134,12 @@ const RubricRatings = ({ feedback, questionId, applicant }) => {
                 className="max-w-md"
                 defaultValue={ratings[questionId]}
                 onChange={(value) => {
-                    console.log("BEFORE")
-                    console.log(ratings)
+                    // console.log("BEFORE")
+                    // console.log(ratings)
                     ratings[questionId] = value;
-                    console.log("AFTER")
-                    console.log(ratings)
-                    updateFeedback();
+                    // console.log("AFTER")
+                    // console.log(ratings)
+                    // updateFeedback();
                 }}
             />
             {/* <Select
@@ -180,6 +196,11 @@ const RubricRatings = ({ feedback, questionId, applicant }) => {
                 //     updateFeedback();
                 // }}
                 onChange={handleCommentsChange} // Update the state when the value changes
+                // onChange={(value) => {
+                //     comments[questionId] = value.target.value;
+                //     setComments(comments)
+                //     updateFeedback();
+                // }} // Update the state when the value changes
                 className="col-span-12 md:col-span-6 mb-6 md:mb-0"
             />
         </>
